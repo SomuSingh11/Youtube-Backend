2f405e7f6e37c49e02e6364ecd6b725736a2061d
SOMU SINGH <somusingh0110@gmail.com>
2024-07-02 19:28:48 +0530

    setup middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// Middleware to enable CORS with options
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // Allow requests from this origin
+    credentials: true, // Allow sending cookies with CORS requests
+  })
+);
+app.use(express.json({ limit: "24kb" })); // Parse JSON requests (limiting payload size to 24kb)
+app.use(express.urlencoded({ extended: true, limit: "24kb" })); // Parse URL-encoded requests (limiting payload size to 24kb)
+app.use(express.static("public")); // Serve static files from the 'public' directory
+app.use(cookieParser()); // Parse cookies attached to the request
 
 export { app };
